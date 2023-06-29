b781120bcf005bf1eff63d3cc3cb25144e24c5b2
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-06-29 15:57:04 +0300

    2nd homework complite

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import { throttle } from 'throttle-debounce';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', throttle(1000, onUpTime));
+
+function onUpTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
