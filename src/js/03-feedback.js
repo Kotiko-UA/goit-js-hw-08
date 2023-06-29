fb70f765079a47d146ab370f2057a361910e8a2b
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-06-29 23:52:55 +0300

    fix task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,18 +5,17 @@ const refers = {
   inputEmail: document.querySelector('.feedback-form input'),
   textAreaEl: document.querySelector('.feedback-form textarea'),
 };
-
+const FORM_LS_VALUE = 'feedback-form-state';
+const savedSettings = localStorage.getItem(FORM_LS_VALUE);
+const parsedSettings = savedSettings ? JSON.parse(savedSettings) : {};
 const currentFormState = {
-  email: '',
-  message: '',
+  email: parsedSettings.email || '',
+  message: parsedSettings.message || '',
 };
 
-const savedSettings = localStorage.getItem('feedback-form-state');
-const parsedSettings = JSON.parse(savedSettings);
-
 if (parsedSettings) {
-  refers.inputEmail.value = parsedSettings.email;
-  refers.textAreaEl.value = parsedSettings.message;
+  refers.inputEmail.value = currentFormState.email;
+  refers.textAreaEl.value = currentFormState.message;
 }
 
 refers.form.addEventListener('input', throttle(500, onInput));
@@ -27,14 +26,14 @@ function onInput(e) {
   } else if (e.target.name === 'message') {
     currentFormState.message = e.target.value;
   }
-  localStorage.setItem('feedback-form-state', JSON.stringify(currentFormState));
+  localStorage.setItem(FORM_LS_VALUE, JSON.stringify(currentFormState));
 }
 
 refers.form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  localStorage.removeItem('feedback-form-state');
+  console.log(JSON.parse(localStorage.getItem(FORM_LS_VALUE)));
+  localStorage.removeItem(FORM_LS_VALUE);
   e.currentTarget.reset();
 }
